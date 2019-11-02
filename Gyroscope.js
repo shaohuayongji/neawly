b9975ba22cae38acea649b3867a14aad948b8e8e
seamly01 <57246513+seamly01@users.noreply.github.com>
2019-11-02 22:43:52 +0800

    Update Gyroscope.js

--- Gyroscope.js
+++ Gyroscope.js
@@ -3,19 +3,16 @@
 */
 
 /*
-Vientiane live download link：https://m.51168.tv/
-Surge4：
-http-response https:\/\/u\.kanghuayun\.com\/api\/v2\/info requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/wxzb.js
+Gyroscope unlock pro (Script author: @Maasea )
+Surge:
+http-response ^https:\/\/api\.gyrosco\.pe\/v1\/account\/$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/gyroscope.js
 
-QX：
-https:\/\/u\.kanghuayun\.com\/api\/v2\/info url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/wxzb.js
+QuantumultX:
+^https:\/\/api\.gyrosco\.pe\/v1\/account\/$ url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/gyroscope.js
 
-Surge & QX MITM = u.kanghuayun.com
+QX & Surge MITM = api.gyrosco.pe
 */
-　　let obj = JSON.parse($response.body);
-　　obj.data.nickname = "脚本禁止牟利,TG频道@NobyDa";
-　　obj.data.tstime = 59169305884;
-　　obj.data.vip_expire_time = 59169305884;
-　　obj.data.tsvip = 1;
-　　obj.data.vip_level = 3;
-　　$done({body: JSON.stringify(obj)});
+
+let obj = JSON.parse($response.body);
+obj.user["active_until_time"] = "2099-01-01T00:00:00Z";
+$done({body: JSON.stringify(obj)});
